db723000152bdde277b1dfa223de4faac77a6328
Afaq Khan <afaqk3203@gmail.com>
2023-07-16 15:58:12 +0500

    created new record on page

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,30 @@
 class EmployeesController < ApplicationController 
 
-def index
-@employees = Employee.all 
-end
+  def index
+      @employees = Employee.all 
+   end
+
+ def new
+    @employee = Employee.new
+
+ end
+
+    def create
+    @employee= Employee.new(employee_params)
+    if @employee.save
+        redirect_to employees_path, notice: "Employee has been created!"
+    else
+        render :new
+      end
+    end
+
+  private 
+   def employee_params 
+    params.require(:employee).permit(:first_name, :last_name,:personal_email, :city, :state, :country , :pincode, :address_line_1, :address_line_2
+    )
+end 
 
 end
+
 
 
