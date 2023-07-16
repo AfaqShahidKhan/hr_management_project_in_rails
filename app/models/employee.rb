b5c9f5d6b1965ae73218d7e45baf333d78dbe9cd
Afaq Khan <afaqk3203@gmail.com>
2023-07-17 01:00:41 +0500

    display errors

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -2,7 +2,7 @@ class Employee < ApplicationRecord
  
     validates :first_name, :last_name, presence: true
     validates :personal_email , presence: true, uniqueness: true 
-    validates :city, :state, :country , :pincode, :address_line_1, presence: true
+    validates :city, :state, :country , :pincode, :address_line_1,  presence: true
 
     def name
         "#{first_name} #{last_name}".strip 
